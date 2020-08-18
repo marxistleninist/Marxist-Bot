3582d9b8bcf0a8c300c06425710b3939829f4c80
Cull Max <68747747+marxistleninist@users.noreply.github.com>
2020-08-18 05:48:05 +0530

    Update bot.js
    commented where necessary

--- bot.js
+++ bot.js
@@ -1,32 +1,35 @@
-const Discord= require('discord.js');
+//discord.js is mandatory, config.json has the prefix and token in it, add your own config file in the form of a JSON object.
+// quiz.json and tflist.json are files that have the questions and statements
 
+const Discord= require('discord.js');
 const {prefix,token}=require('./config.json');
+const quiz= require('./quiz.json'); 
+const tflist=require('./tflist.json');
+
 
 const client=new Discord.Client();
-const quiz= require('./quiz.json');
 
 
 
 client.once('ready',()=>{
 	console.log('Bot is now connected');
-	//client.channels.find(x=>x.name==='general').send('Hello Master! I will work for you.');
-	
 });
 
 
+// This embed is used to show 'help' menu
 
-const exampleEmbed = {
+const helpEmbed = {
 	color: 0x0099ff,
 	title: 'Marxist Bot',
-	url: 'https://marxistleninist.github.io',
+	url: 'https://github.com/marxistleninist/Marxist-Bot',
 	author: {
 		name: 'Help',
 		icon_url: 'https://i.imgur.com/hNIVvJ3.jpg',
-		url: 'https://github.com/marxistleninist/Marxist-Bot',
+		url: 'https://marxistleninist.github.io',
 	},
 	description: 'This bot will help you learn Communist Theory and Communist History in a fun way.',
 	thumbnail: {
-		url: 'https://i.imgur.com/VwpFNkw.jpg',
+		url: 'https://i.imgur.com/T9fUcCQ.jpeg',
 	},
 	fields: [
 		{
@@ -36,13 +39,16 @@ const exampleEmbed = {
 		},
 		{
 			name: '!quiz',
-			value: 'This command will ask you questions.',
+			value: 'This command will ask you questions with four options.',
+			inline: true,
+		},
+		{
+			name: '!tf',
+			value: 'This command will ask you true/false questions.',
 			inline: true,
 		},
 	],
-	image: {
-		url: 'https://i.imgur.com/T9fUcCQ.jpeg',
-	},
+	
 	timestamp: new Date(),
 	footer: {
 		text: 'Bot started ',
@@ -50,39 +56,71 @@ const exampleEmbed = {
 	},
 };
 
-const ques = {
-	color: 0x0099ff,
-	fields: [
-		{
-			name: 'Question',
-			value: '',
-			inline: true,
-		}
-	],
+// This embed is used for questions,title, description and footer text are updated according to the type of question
+const question = {
+	color: 0xff0000,
+	title: '',
+	thumbnail: {
+		url: 'https://i.imgur.com/T9fUcCQ.jpeg',
+	},
+	description:"",
 
 	footer: {
-		text: 'Category: CPI History'
+		text: ''
 	},
 };
 
 
-
 client.on('message',message=>{
-	if((message.content===('f') || message.content===('F')) && !message.author.bot)
-		message.channel.send();
-
+	
+	// If the user writes '!quiz then this block runs
 	if(message.content.startsWith(prefix+'quiz')){
-
+		
+			//select a random question from the array
 			const item = quiz[Math.floor(Math.random() * quiz.length)];
-			console.log(item["options"][0]);
+			
+			// prepare the list of options
 			let options="A) "+item["options"][0]+'\n'+"B) "+item["options"][1]+'\n'+"C) "+item["options"][2]+'\n'+"D) "+item["options"][3];
+				
+				// This filter is applied so that the person sending first and second message can be identified.
 				const filter = m => m.author.id===message.author.id;
-				//const collector = message.channel.createMessageCollector(filter, { time: 15000, max: 1, maxMatches: 1 });
-
-				ques.fields[0].value= item.question+"\n"+options;
-				message.channel.send({ embed: ques }).then(() => {
-					message.channel.awaitMessages(filter, { max: 1, time: 10000, errors: ['time'] })
+				
+				question.title="Question";
+				question.description=item.question+"\n"+options;
+				question.footer.text="Category: "+item.category;
+				
+				//here we send the embed and wait for the reply from the user
+				message.channel.send({ embed: question }).then(() => {
+					message.channel.awaitMessages(filter, { max: 1, time: 12000, errors: ['time'] })
 							.then(collected => {
+						//if the answer is correct then show message else show the correct answer		
+						if(collected.first().content.toLowerCase()===item.answer)
+							message.channel.send('You got the correct answer!');
+						else message.channel.send('Wrong answer!!!'+ " The correct option is: ```"+item.answer.toUpperCase()+") "+item["options"][item.answer.toUpperCase().charCodeAt(0)-65]+"```");
+				})
+					// In case if the user doesnt reply within the time limit
+				.catch(collected => {
+				message.channel.send('Time\'s up!!!');
+			});
+		});
+
+	}
+	//same as above
+	else if(message.content.startsWith(prefix+'tf')){
+			
+			const item = tflist[Math.floor(Math.random() * tflist.length)];
+			
+			
+				const filter = m => m.author.id===message.author.id;
+				
+				question.title="True or False";
+				question.description= item.statement;
+				question.footer.text="Type t/f";
+
+				message.channel.send({ embed: ques }).then(() => {
+					message.channel.awaitMessages(filter, { max: 1, time: 12000, errors: ['time'] })
+							.then(collected => {
+								console.log(collected.first().content.toLowerCase());
 								if(collected.first().content.toLowerCase()===item.answer)
 						message.channel.send('You got the correct answer!');
 					else message.channel.send('Wrong answer!!!');
@@ -90,37 +128,13 @@ client.on('message',message=>{
 				.catch(collected => {
 				message.channel.send('Time\'s up!!!');
 			});
-		});
-
-
-
-
-
-
-
-
-
-
-
-				// collector.on('collect', m => {
-					
-				// 	if(m.content===item.answer)message.channel.send('Kya baat! Sab jante hai aap!');
-				// 	else message.channel.send('Padh ke aao bey');
-					
-				// });
-
-				// collector.on('end', collected => {
-				// 	message.channel.send('Times up! You are too fast man!');
-				// });
+		});		
 	}
-	if(message.content.startsWith(prefix+'help')){
+	else if(message.content.startsWith(prefix+'help')){
 		message.channel.send({ embed: exampleEmbed });
 
 	}
-	else if(message.content.startsWith(prefix+'info')){
-		message.channel.send('Id was created '+message.author.createdAt+
-			'\nYour last msg '+message.author.lastMessage);
-	}
+	
 
 });
 
